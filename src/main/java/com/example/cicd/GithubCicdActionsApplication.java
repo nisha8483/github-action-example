5850cb7ccf1ac60b4f11d7c92f4d57d2b3c431a9
Nisha <nisha.agrawal83@outlook.com>
2023-09-25 14:10:53 -0500

    code

---
+++ src/main/java/com/example/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,27 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+@GetMapping("/welcome")
+	public String welcome(){
+	return "Welcome to CICD example";
+
+	}
+/*	echo "# github-action-example" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/nisha8483/github-action-example.git
+	git push -u origin main */
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
